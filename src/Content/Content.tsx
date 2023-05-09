26aaff81ba1c13403820316bac81127abc207e76
Scharmi <scharmi1500@gmail.com>
2023-05-09 14:48:09 +0200

    Developed all layers of page

--- src/Content/Content.tsx
+++ src/Content/Content.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { TextField } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import './Content.css';
 import List from './List/List';
 import sampleData from './sampleData.json'
@@ -8,11 +8,37 @@ import sampleData from './sampleData.json'
 export interface ContentProps {
     layer: "chooseCompany" | "chooseMedicine" | "viewMedicine"
     setLayer: Function;
+    parameters: { company: string, medicine: string };
+    setParameters: Function;
 }
+
 function Content(props: ContentProps) {
     const layer = props.layer
     const [searchText, setSearchText] = useState<string>("")
     const [content, setContent] = useState<Array<any>>(sampleData.sampleCompanies)
+    useEffect(() => {
+        if(layer === "chooseMedicine") setContent(sampleData.sampleMedicines);
+        if(layer === "chooseCompany") setContent(sampleData.sampleCompanies);
+    }, [layer])
+    
+
+    let onClickFunction;
+    switch (layer) {
+        case "chooseCompany":
+            onClickFunction = (name: string) => {
+                props.setParameters({...props.parameters, company: name })
+                props.setLayer("chooseMedicine")
+            }
+        break;
+        case "chooseMedicine":
+            onClickFunction = (name: string) => {
+                props.setParameters({...props.parameters, medicine: name })
+                props.setLayer("viewMedicine")
+            }
+        break;
+        default:
+            onClickFunction = () => {}
+    }
     useEffect(() => {
         //Tutaj będzie request do backendu zakończony wywołaniem setContent
         console.log("Backend request sent: " + props.layer)
@@ -26,8 +52,16 @@ function Content(props: ContentProps) {
             return element.name.toLowerCase().includes(substring.toLowerCase())
         })
     }
+
     return (
         <div className="Content">
+            <div className="returnButton">
+                <Button variant="contained" 
+                    onClick={() => {props.setParameters({company: "", medicine: ""}); props.setLayer("chooseCompany")}}
+                >
+                Powrót do strony głównej
+                </Button>
+            </div>
             <div className="searchBar">
                 <TextField
                     id="outlined-helperText"
@@ -37,7 +71,7 @@ function Content(props: ContentProps) {
                 />
             </div>
             <div className="table">
-                <List content={filter(content, searchText)} layer={layer}/>
+                <List onClickFunction={onClickFunction} content={filter(content, searchText)} layer={layer}/>
             </div>
         </div>
     );
--- src/Content/List/List.tsx
+++ src/Content/List/List.tsx
@@ -4,30 +4,54 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import './List.css';
 export interface TableProps {
     content: Array<any>
     layer?: "chooseCompany" | "chooseMedicine" | "viewMedicine"
+    onClickFunction?: Function
 }
 function List(props: TableProps) {
+    const onClickFunction = props.onClickFunction ? props.onClickFunction : () => {console.log("No onClickFunction provided")};
+    const clickable = () => {
+        switch (props.layer) {
+            case "chooseCompany":
+                return ["name"]
+            case "chooseMedicine":
+                return ["name"]
+            case "viewMedicine":
+                return ["name"]
+            default:
+                return ["name"]
+        }
+    }
     const layerHeaders = (layer: "chooseCompany" | "chooseMedicine" | "viewMedicine") => {
         switch (layer) {
             case "chooseCompany":
                 return ["name", "id"]
             case "chooseMedicine":
-                return ["lek", "id"]
+                return ["name", "id"]
             case "viewMedicine":
                 return ["name", "id"]
         }
     }
+    const displayNames = {
+        name: (props.layer === "chooseCompany" ? "Nazwa firmy" : "Nazwa leku"),
+        id: "ID",
+    }
+    const displayName = (header: string) => header in displayNames ? displayNames[header as keyof typeof displayNames] : header
     const headers = props.layer ? layerHeaders(props.layer) : Object.keys(props.content[0])
     const renderTableHead = () => {
-        return headers.map((header, index) => {
-            return <TableCell key={index}>{header.charAt(0).toUpperCase() + header.slice(1)}</TableCell>
+        return headers.map((header:string, index) => {
+            return <TableCell key={index}>{displayName(header)}</TableCell>
         })
     }
     const renderTableRow = (element: any) => {
         return headers.map((header, index) => {
-            return <TableCell key={index}>{element[header]}</TableCell>
+            return (
+                clickable().includes(header) ?
+                <TableCell key={index}><div className="clickable" onClick={() => onClickFunction(header)}>{element[header]}</div></TableCell> :
+                <TableCell key={index}>{element[header]}</TableCell>     
+            )
         })
     }
     const renderTableBody = () => {
